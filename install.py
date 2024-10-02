db35a596c3aec740c5568ca2b40a0f8a1d6b37c9
3222h <394954a+++@gmail.com>
2024-10-02 09:24:25 +0530

    Update install.py

--- install.py
+++ install.py
@@ -1,26 +1,18 @@
-#!/bin/bash
-# Installation script for autoTOR
+import os
+choice = input('[+] to install press (Y) to uninstall press (N) >> ')
+run = os.system
+if str(choice) =='Y' or str(choice)=='y':
 
-echo "[+] Do you want to install Auto TOR IP changer? (y/n)"
-read choice
+    run('chmod 777 autoTOR.py')
+    run('mkdir /usr/share/aut')
+    run('cp autoTOR.py /usr/share/aut/autoTOR.py')
 
-if [[ $choice == "Y" || $choice == "y" ]]; then
-    # Give execution permissions to the Python file
-    chmod 777 autoTOR.py
-
-    # Create the directory and move the Python file there
-    mkdir -p /usr/share/aut/
-    cp autoTOR.py /usr/share/aut/
-
-    # Create a shell script in /usr/bin/ to allow running the Python file via 'aut' command
-    echo "#!/bin/bash" > /usr/bin/aut
-    echo "python3 /usr/share/aut/autoTOR.py" >> /usr/bin/aut
-
-    # Set the necessary permissions
-    chmod +x /usr/bin/aut
-    chmod +x /usr/share/aut/autoTOR.py
-
-    echo "[+] Auto TOR IP changer installed successfully. Run it using the command: aut"
-else
-    echo "[+] Installation aborted."
-fi
+    cmnd=(' #! /bin/sh \n exec python3 /usr/share/aut/autoTOR.py "$@"')
+    with open('/usr/bin/aut','w')as file:
+        file.write(cmnd)
+    run('chmod +x /usr/bin/aut & chmod +x /usr/share/aut/autoTOR.py')
+    print('''\n\ncongratulation auto Tor Ip Changer is installed successfully \nfrom now just type \x1b[6;30;42maut\x1b[0m in terminal ''')
+if str(choice)=='N' or str(choice)=='n':
+    run('rm -r /usr/share/aut ')
+    run('rm /usr/bin/aut ')
+    print('[!] now Auto Tor Ip changer  has been removed successfully')
